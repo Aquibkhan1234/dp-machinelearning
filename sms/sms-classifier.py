053c42df8fcf9b31b1679bcf53ae1a0861ee04ef
Aquibkhan1234 <141300872+Aquibkhan1234@users.noreply.github.com>
2024-11-09 18:48:53 +0530

    Update sms-classifier.py

--- sms/sms-classifier.py
+++ sms/sms-classifier.py
@@ -7,7 +7,7 @@ from nltk.stem.porter import PorterStemmer
 
 ps = PorterStemmer()
 
-
+st.header("Spam"/"Ham)"
 def transform_text(text):
     text = text.lower()
     text = nltk.word_tokenize(text)
