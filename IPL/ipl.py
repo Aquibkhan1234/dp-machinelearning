897ab8ac02e4d09c3b19a62b3201b4642d5b24bc
Aquibkhan1234 <141300872+Aquibkhan1234@users.noreply.github.com>
2024-11-13 23:31:29 +0530

    Update ipl.py

--- IPL/ipl.py
+++ IPL/ipl.py
@@ -18,7 +18,7 @@ cities = ['Hyderabad', 'Bangalore', 'Mumbai', 'Indore', 'Kolkata', 'Delhi',
        'Visakhapatnam', 'Pune', 'Raipur', 'Ranchi', 'Abu Dhabi',
        'Sharjah', 'Mohali', 'Bengaluru']
 
-pipe = pickle.load(open('ipl.pkl', 'rb'))
+pipe = pickle.load(open('IPL/ipl.pkl', 'rb'))
 st.title("IPL Win Predictor")
 
 col1, col2 = st.columns(2)
