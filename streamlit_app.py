5ccd067c70dd31fe4580a90165fc3000a451da02
Aquibkhan1234 <141300872+Aquibkhan1234@users.noreply.github.com>
2024-11-17 12:41:29 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from sklearn.ensemble import RandomForestClassifier
-st.title('🎈Machine learning App ')
+st.title('🤖 Machine learning App ')
 
 st.info("This is app builds a machine learning model!")
 
